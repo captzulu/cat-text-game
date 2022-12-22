4b322e2883c92f4cc234543298c4281b46b7aa0b
Max <lhmax55@gmail.com>
2022-12-22 01:25:29 -0500

    a map composer of rows of nodes with autoGeneration

--- gameObjects/sections/map/map.py
+++ gameObjects/sections/map/map.py
@@ -1,5 +1,34 @@
-from dataclasses import dataclass
 from gameObjects.sections.map.node import Node
-@dataclass
+import itertools
+
 class Map():
-    nodes: dict[int, Node]
+    nodes : dict[int, list[Node]] = dict()
+    nodeIdGenerator = itertools.count()
+    
+    def __init__(self):
+        return
+    
+    def autoGenerateNode(self, rowIndex : int) -> int:
+        name = Node.randomName()
+        previousRow = self.getRowAtIndex(rowIndex - 1)
+        backLinks = Node.randomBackLinks(self.getRowIds(previousRow))
+        
+        if rowIndex not in self.nodes:
+            self.nodes[rowIndex] = list()
+
+        newNode = Node(next(self.nodeIdGenerator), name, rowIndex, backLinks)
+        self.nodes[rowIndex].append(newNode)
+        return newNode.id
+    
+    def getRowAtIndex(self, rowIndex : int) -> list[Node]:
+        if rowIndex < 0 or rowIndex not in self.nodes:
+            return []
+        return self.nodes[rowIndex]
+    
+    def getRowIds(self, row : list[Node]) -> list[int]:
+        ids : list[int] = list()
+        for node in row:
+            ids.append(node.id)
+        return ids
+    
+    
--- tests/gameObjectsTest/map_test.py
+++ tests/gameObjectsTest/map_test.py
@@ -5,9 +5,30 @@ from gameObjects.sections.map.node import Node
 class mapTest(unittest.TestCase):
 
     def testInitClass(self):
-        newMap = Map({0 : Node(1, 'node 1', [], [2])})
+        newMap = Map()
         self.assertIsInstance(newMap, Map)
+    
+    def testAutoGenerate(self):
+        newMap = Map()
+        rowIndex = 0
+        actualId = newMap.autoGenerateNode(rowIndex)
+        expectedId = 0
+        self.assertEqual(len(newMap.nodes), 1)
+        self.assertEqual(actualId, expectedId)
         
+    def testAutoGenerate_twoRows_incrementingId(self):
+        newMap = Map()
+        newMap.autoGenerateNode(0)
+        actualId = newMap.autoGenerateNode(1)
+        expectedId = 1
+        self.assertEqual(actualId, expectedId)
+        
+    def testAutoGenerate_twoRows_linkedNodes(self):
+        newMap = Map()
+        expectedId = newMap.autoGenerateNode(0)
+        newMap.autoGenerateNode(1)
+        actualId = newMap.nodes[1][0].backLinks
+        self.assertEqual(actualId, [expectedId])       
         
 
 if __name__ == '__main__':
