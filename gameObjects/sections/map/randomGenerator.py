5428f8a7441f60de59800a87e3afdb5facc63a42
captzulu <lhmax55@gmail.com>
2022-12-29 19:48:28 +0000

    comment for refactoring

--- gameObjects/sections/map/randomGenerator.py
+++ gameObjects/sections/map/randomGenerator.py
@@ -5,6 +5,7 @@ class RandomGenerator():
     def __init__(self) -> None:
         pass
     
+    #should be moves in Node as a classMethod like generateRandomMap in Map
     def generateRandomNode(self, previousColumnIds: list[int], id : int, columnIndex : int) -> Node:
         name = Node.randomName()
         backLinks = Node.randomBackLinks(previousColumnIds)
