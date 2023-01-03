97deda821b4f063b624a0c05ad8958fbeeb8f259
captzulu <lhmax55@gmail.com>
2023-01-03 06:03:06 +0000

    add executeBattle to wrap all + make all battle function private

--- gameObjects/sections/battle/battle.py
+++ gameObjects/sections/battle/battle.py
@@ -13,43 +13,48 @@ class Battle:
     completed: bool = False
     edgeSymbol: str = '°'
     Filler: str = '='
+    
+    def executeBattle(self):
+        self.__executeIntro()
+        input("Hit a key to start the fight...")
+        self.__battleLoop()
 
-    def write(self, text : str):
-        #self.log = BattleLog()
+    def __executeIntro(self):
+        title = f"Battle ! {self.side1.name} Vs {self.side2.name}"
+        titleLine = self.edgeSymbol + self.fillTitleLine(title) + self.edgeSymbol
+        self.__write(titleLine)
+        self.__write(str(self.side1.getActiveMonSpecies()))
+        
+        longestMonNameLength = self.__calculateLongestMonNameLength()
+        paddingLength = longestMonNameLength // 2
+        self.__write(' ' * paddingLength + 'VS' + (' ' * paddingLength))
+        self.__write(str(self.side2.getActiveMonSpecies()))
+        self.__write(self.edgeSymbol + (self.Filler * longestMonNameLength) + self.edgeSymbol)
+        print(self.__getTurnLog())
+
+    def fillTitleLine(self, title:str) -> str:
+        longestMonNameLength = self.__calculateLongestMonNameLength()
+        titleLineHalf : str = self.Filler * ((longestMonNameLength - len(title)) // 2)
+        return titleLineHalf + title + titleLineHalf
+
+    def __write(self, text : str):
         if hasattr(self, 'log') == False:
             self.log = BattleLog()
         self.log.addExplicitLine(self.turn, text)
         self.log.addImplicitLine(self.turn, text)
-
-    def executeIntro(self):
-        title = f"Battle ! {self.side1.name} Vs {self.side2.name}"
-        titleLine = self.edgeSymbol + self.fillTitleLine(title) + self.edgeSymbol
-        self.write(titleLine)
-        self.write(str(self.side1.getActiveMonSpecies()))
-        
-        longestMonNameLength = self.calculateLongestMonNameLength()
-        paddingLength = longestMonNameLength // 2
-        self.write(' ' * paddingLength + 'VS' + (' ' * paddingLength))
-        self.write(str(self.side2.getActiveMonSpecies()))
-        self.write(self.edgeSymbol + (self.Filler * longestMonNameLength) + self.edgeSymbol)
-
-    def fillTitleLine(self, title:str) -> str:
-        longestMonNameLength = self.calculateLongestMonNameLength()
-        titleLineHalf : str = self.Filler * ((longestMonNameLength - len(title)) // 2)
-        return titleLineHalf + title + titleLineHalf
     
-    def calculateLongestMonNameLength(self) -> int:
+    def __calculateLongestMonNameLength(self) -> int:
         mon1Length = len(str(self.side1.getActiveMonSpecies()))
         mon2Length = len(str(self.side2.getActiveMonSpecies()))
         return mon1Length if mon1Length > mon2Length else mon2Length
 
-    def executeTurn(self):
+    def __executeTurn(self):
         self.turn += 1
-        self.write(f"=== New turn ({self.turn}) ===")
+        self.__write(f"=== New turn ({self.turn}) ===")
         self.__attackPhase()
         if self.turn >= 100:
             winner = self.side1.activeMon if self.side1.activeMon.currentHealth > self.side2.activeMon.currentHealth else self.side2.activeMon
-            self.completeBattle(f'{winner.nickname} has stalled out the win !')
+            self.__completeBattle(f'{winner.nickname} has stalled out the win !')
             
     def __attackPhase(self):
         side1Speed = self.side1.activeMon.speed
@@ -62,34 +67,42 @@ class Battle:
             firstSide = self.side1 if side1Speed > side2Speed else self.side2
             secondSide = self.side2 if side1Speed > side2Speed else self.side1
         self.__sideTurn(firstSide, secondSide)
-        if self.hasCompleted() == False:
+        if self.__hasCompleted() == False:
             self.__sideTurn(secondSide, firstSide)
 
     def __sideTurn(self, side : Side, oppositeSide : Side):
         oppositeMon = oppositeSide.activeMon
         self.__attack(side.activeMon, oppositeMon, side.activeMon.genericMon.type1)
         if oppositeSide.isDefeated():
-            self.completeBattle(f'{oppositeMon.nickname} has fainted !')
+            self.__completeBattle(f'{oppositeMon.nickname} has fainted !')
         else:
-            self.write(f'{oppositeMon.nickname} has {oppositeMon.currentHealth}/{oppositeMon.maxHealth} health !')
+            self.__write(f'{oppositeMon.nickname} has {oppositeMon.currentHealth}/{oppositeMon.maxHealth} health !')
 
-    def hasCompleted(self):
+    def __battleLoop(self) -> None:
+        while self.__hasCompleted() == False:
+            self.__executeTurn()
+            print(self.__getTurnLog())
+            if self.__hasCompleted() == False:
+                input("Hit a key to continue...")
+        return
+
+    def __hasCompleted(self):
         return self.completed
             
-    def completeBattle(self, message:str):
-        self.write(message)
+    def __completeBattle(self, message:str):
+        self.__write(message)
         self.completed = True
         return
     
     def __attack(self, attacker:SpecificMon, defender:SpecificMon, attackType:Type):
         damage = int(attacker.attack * defender.weakTo(attackType))
         defender.loseHealth(damage)
-        self.write(f"{attacker.nickname} dealt {damage} to {defender.nickname}")
+        self.__write(f"{attacker.nickname} dealt {damage} to {defender.nickname}")
         return
         
-    def writeImplicit(self, text : str):
+    def __writeImplicit(self, text : str):
         self.log.addImplicitLine(self.turn, text)
         
-    def getTurnLog(self):
+    def __getTurnLog(self):
         return self.log.getFormattedLine(self.turn)
     
