9a856f2acd2ca7bc9440005032c24b0a5af6ab9e
Max <lhmax55@gmail.com>
2023-01-03 20:46:01 -0500

    fix tests

--- gameObjects/sections/battle/battle.py
+++ gameObjects/sections/battle/battle.py
@@ -22,35 +22,35 @@ class Battle:
     def __executeIntro(self):
         title = f"Battle ! {self.side1.name} Vs {self.side2.name}"
         titleLine = self.edgeSymbol + self.fillTitleLine(title) + self.edgeSymbol
-        self.__write(titleLine)
-        self.__write(str(self.side1.getActiveMonSpecies()))
+        self.write(titleLine)
+        self.write(str(self.side1.getActiveMonSpecies()))
         
-        longestMonNameLength = self.__calculateLongestMonNameLength()
+        longestMonNameLength = self.calculateLongestMonNameLength()
         paddingLength = longestMonNameLength // 2
-        self.__write(' ' * paddingLength + 'VS' + (' ' * paddingLength))
-        self.__write(str(self.side2.getActiveMonSpecies()))
-        self.__write(self.edgeSymbol + (self.Filler * longestMonNameLength) + self.edgeSymbol)
+        self.write(' ' * paddingLength + 'VS' + (' ' * paddingLength))
+        self.write(str(self.side2.getActiveMonSpecies()))
+        self.write(self.edgeSymbol + (self.Filler * longestMonNameLength) + self.edgeSymbol)
         print(self.__getTurnLog())
 
     def fillTitleLine(self, title:str) -> str:
-        longestMonNameLength = self.__calculateLongestMonNameLength()
+        longestMonNameLength = self.calculateLongestMonNameLength()
         titleLineHalf : str = self.Filler * ((longestMonNameLength - len(title)) // 2)
         return titleLineHalf + title + titleLineHalf
 
-    def __write(self, text : str):
+    def write(self, text : str):
         if hasattr(self, 'log') == False:
             self.log = BattleLog()
         self.log.addExplicitLine(self.turn, text)
         self.log.addImplicitLine(self.turn, text)
     
-    def __calculateLongestMonNameLength(self) -> int:
+    def calculateLongestMonNameLength(self) -> int:
         mon1Length = len(str(self.side1.getActiveMonSpecies()))
         mon2Length = len(str(self.side2.getActiveMonSpecies()))
         return mon1Length if mon1Length > mon2Length else mon2Length
 
     def __executeTurn(self):
         self.turn += 1
-        self.__write(f"=== New turn ({self.turn}) ===")
+        self.write(f"=== New turn ({self.turn}) ===")
         self.__attackPhase()
         if self.turn >= 100:
             winner = self.side1.activeMon if self.side1.activeMon.currentHealth > self.side2.activeMon.currentHealth else self.side2.activeMon
@@ -76,7 +76,7 @@ class Battle:
         if oppositeSide.isDefeated():
             self.__completeBattle(f'{oppositeMon.nickname} has fainted !')
         else:
-            self.__write(f'{oppositeMon.nickname} has {oppositeMon.currentHealth}/{oppositeMon.maxHealth} health !')
+            self.write(f'{oppositeMon.nickname} has {oppositeMon.currentHealth}/{oppositeMon.maxHealth} health !')
 
     def __battleLoop(self) -> None:
         while self.__hasCompleted() == False:
@@ -90,17 +90,17 @@ class Battle:
         return self.completed
             
     def __completeBattle(self, message:str):
-        self.__write(message)
+        self.write(message)
         self.completed = True
         return
     
     def __attack(self, attacker:SpecificMon, defender:SpecificMon, attackType:Type):
         damage = int(attacker.attack * defender.weakTo(attackType))
         defender.loseHealth(damage)
-        self.__write(f"{attacker.nickname} dealt {damage} to {defender.nickname}")
+        self.write(f"{attacker.nickname} dealt {damage} to {defender.nickname}")
         return
         
-    def __writeImplicit(self, text : str):
+    def writeImplicit(self, text : str):
         self.log.addImplicitLine(self.turn, text)
         
     def __getTurnLog(self):
