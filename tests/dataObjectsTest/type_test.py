d04b9bda392451840077860355c64693e3c9f57f
Max <lhmax55@gmail.com>
2023-01-03 22:45:10 -0500

    test check type modifier all outcome

--- tests/dataObjectsTest/type_test.py
+++ tests/dataObjectsTest/type_test.py
@@ -6,13 +6,25 @@ class typeTest(unittest.TestCase):
     def testInitClass(self):
         newType = self.createType()
         self.assertIsInstance(newType, Type)
-        
-    def testGetHitDamageModifier(self):
+
+    def testCheckTypeModifier_superEffective(self):
         newType = self.createType()
         self.assertEqual(newType.checkTypeModifier("dark"), 2)
-        
-    def createType(self) -> Type:
-        return Type("bob", "dark", "psy", "", "")
 
+    def testCheckTypeModifier_resisted(self):
+        newType = self.createType()
+        self.assertEqual(newType.checkTypeModifier("psy"), 0.5)
+
+    def testCheckTypeModifier_neutral(self):
+        newType = self.createType()
+        self.assertEqual(newType.checkTypeModifier("fake"), 1)
+        
+    def testCheckTypeModifier_immune(self):
+        newType = self.createType()
+        self.assertEqual(newType.checkTypeModifier("wat"), 0)
+
+    def createType(self) -> Type:
+        return Type("bob", "dark", "psy", "wat", "")
+ # type: ignore
 if __name__ == '__main__':
     unittest.main()
