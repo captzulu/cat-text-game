641caa0408485819e339501d45f7c7c350618dc8
captzulu <lhmax55@gmail.com>
2023-01-04 19:11:37 +0000

    add typing

--- gameObjects/sections/map/node.py
+++ gameObjects/sections/map/node.py
@@ -4,18 +4,18 @@ class Node():
     NAME_LIST = ['city', 'market', 'fight', 'chickens', 'rest']
     
     def __init__(self, id : int, name : str, columnIndex : int, backLinks : list[int], forwardLinks : list[int]=list()):
-        self.id = id
-        self.name = name 
-        self.backLinks = backLinks 
-        self.forwardLinks = forwardLinks
-        self.columnIndex = columnIndex
+        self.id : int = id
+        self.name : str = name 
+        self.backLinks : list[int] = backLinks 
+        self.forwardLinks : list[int] = forwardLinks
+        self.columnIndex : int = columnIndex
     
     def __str__(self) -> str:
         return f"{{{self.id}:{self.name} {self.backLinks}}}"
 
     @staticmethod
     def randomName() -> str:
-        results = random.choices(Node.NAME_LIST, weights=[1, 1, 8, 1, 5])
+        results : list[str] = random.choices(Node.NAME_LIST, weights=[1, 1, 8, 1, 5])
         return results[0]
 
     @staticmethod
@@ -23,10 +23,10 @@ class Node():
         if len(previousIds) == 0:
             return []
 
-        amount = random.choices([1, 2, 3], weights=[2, 5, 3])[0]
-        results = list()
+        amount : int = random.choices([1, 2, 3], weights=[2, 5, 3])[0]
+        results : list[int] = list()
         while amount > 0 and len(previousIds) > 0:
-            pickedId = random.choice(previousIds)
+            pickedId: int = random.choice(previousIds)
             results.append(pickedId)
             previousIds.remove(pickedId)
             amount -= 1
