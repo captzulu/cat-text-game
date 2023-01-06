8eae0709d60252ab88038ead1ceef1e7e627da95
Max <lhmax55@gmail.com>
2023-01-05 23:28:45 -0500

    test

--- tests/dataObjectsTest/attack_test.py
+++ tests/dataObjectsTest/attack_test.py
@@ -1,16 +1,24 @@
 import unittest
+import _globals
+from dataFactory import dataFactory
 from dataObjects.attack import Attack
-from dataObjects.type import Type 
 
 class attackTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.initTestTypes()
+    
+    def initTestTypes(self):
+        _globals.types = dataFactory.loadClassDictTest('type')
+        
+    def testLoadFile(self):
+        _globals.types = dataFactory.loadClassDict('type')
+        attacks : dict[str, object] = dataFactory.loadClassDict('attack')
+        self.assertIsInstance(attacks['0'], Attack)
 
     def testInitClass(self):
-        newAttack = Attack("Slash", 70, self.createType())
+        newAttack = Attack("Slash", 70, '1')
         self.assertIsInstance(newAttack, Attack)
         
-    def createType(self) -> Type:
-        return Type("bob", "dark", "psy", "", "")
-        
-
 if __name__ == '__main__':
     unittest.main()
