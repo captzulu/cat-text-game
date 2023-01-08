639bf1762754aa2d70adffcfa5415f1ce9163164
Max <lhmax55@gmail.com>
2023-01-07 19:20:27 -0500

    add moves to globals

--- _globals.py
+++ _globals.py
@@ -1,7 +1,9 @@
+from dataObjects.move import Move
 from dataObjects.type import Type
 from dataObjects.genericMon import GenericMon
 from screenObjects.screenObject import ScreenObject
 types: dict[str, Type] = {}
+moves: dict[str, Move] = {}
 genericMons: dict[str | int, GenericMon] = {}
 playerName: str = 'trainer'
 X: int = 600
