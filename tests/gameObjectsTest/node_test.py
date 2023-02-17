9b1166cf33d2a12d141a4436b859fb3b17d6599d
Max <lhmax55@gmail.com>
2023-02-16 23:21:51 -0500

    migrate tests

--- tests/gameObjectsTest/node_test.py
+++ tests/gameObjectsTest/node_test.py
@@ -7,20 +7,6 @@ class nodeTest(unittest.TestCase):
         newNode =  Node(1, 'node 1', 0, [])
         self.assertIsInstance(newNode, Node)
         
-    def testRandomName(self):
-        self.assertIsInstance(Node.randomName(), str)
-    
-    def testRandomBackLinks_emptyPrevious(self):
-        self.assertIsInstance(Node.randomBackLinks([]), list)
-
-    def testRandomBackLinks_PopulatedPrevious(self):
-        self.assertIsInstance(Node.randomBackLinks([1, 2, 3]), list)
-        
-    def testGenerateRandomNode(self):
-        columnIndex = 0
-        node = Node.generateRandomNode(3, columnIndex, [0, 1, 2])
-        self.assertIsInstance(node, Node)
-        
 if __name__ == '__main__':
     unittest.main()
     
