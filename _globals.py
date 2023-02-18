490cd54888df625688b843b9fcd395606591aa80
maxime <lhmax55@gmail.com>
2023-02-18 12:17:32 -0500

    delete any remnants of pygame

--- _globals.py
+++ _globals.py
@@ -2,12 +2,9 @@ from dataObjects.move import Move
 from dataObjects.type import Type
 from dataObjects.genericMon import GenericMon
 from gameObjects.sections.player.player import Player
-from screenObjects.screenObject import ScreenObject
 types: dict[str, Type] = {}
 moves: dict[str, Move] = {}
 genericMons: dict[str | int, GenericMon] = {}
 player: Player
 X: int = 600
 Y: int = 400
-objects: dict[str, ScreenObject] = {}
-clickables: list[ScreenObject] = []
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import pygame
-import ext_modules.ptext as ptext
 from dataFactory import dataFactory
 import _globals  
-from game import Game
 from dataObjects.enums.gameStates import GameStates
 from cliObjects.gameCli import GameCli
 
@@ -16,16 +13,6 @@ def main():
     #playerName = _globals.playerName = input("What's your name? > ").capitalize()
     #print(f'Your name is {playerName}')
     withCli()
-    #menuFunctions.menuCallable({1 : ("Pygame", withPygame), 2 : ("CLI", withCli)})
-
-def withPygame():
-    pygame.init()
-    pygame.display.set_caption("Pokemon Clone")
-    #print(pygame.font.get_fonts())
-    ptext.DEFAULT_FONT_NAME = pygame.font.match_font('dejavusansmono')
-    game = Game()
-    while game.gameState == GameStates.RUNNING:
-        game.run()
         
 def withCli():
     game = GameCli()
