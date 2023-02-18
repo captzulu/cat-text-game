a388da47c57048a5d062cddaae30bb61f6c25c13
maxime <lhmax55@gmail.com>
2023-02-18 12:17:53 -0500

    make randomFactory static

--- cliObjects/gameCli.py
+++ cliObjects/gameCli.py
@@ -3,9 +3,7 @@ import random
 from cliObjects.menuFunctions import menuFunctions
 from dataObjects.enums.gameStates import GameStates
 from gameObjects.sections.player.player import Player
-from gameObjects.sections.battle.battle import Battle
-from gameObjects.specificMon import SpecificMon
-from gameObjects.sections.battle.side import Side
+from gameObjects.sections.map.randomFactory import RandomFactory
 from gameObjects.sections.map.map import Map
 from gameObjects.sections.map.nodeEvents import NodeEvents
 from typing import Callable
@@ -14,7 +12,7 @@ class GameCli:
     map : Map 
     def __init__(self) -> None:
         self.gameState : GameStates = GameStates.RUNNING
-        self.map = Map.generateRandomMap(10, "new map")
+        self.map = RandomFactory().generateRandomMap(10, "new map")
 
     def quit(self):
         self.gameState = GameStates.ENDED
