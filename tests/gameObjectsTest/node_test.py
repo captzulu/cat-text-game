c01f29d1909849ca1c124f0a2385b502e4f28090
Max <lhmax55@gmail.com>
2023-02-22 00:07:47 -0500

    Continue using nodeType enum

--- tests/gameObjectsTest/node_test.py
+++ tests/gameObjectsTest/node_test.py
@@ -1,10 +1,11 @@
 import unittest
 from gameObjects.sections.map.node import Node
+from dataObjects.enums.nodeType import NodeType
 
 class nodeTest(unittest.TestCase):
 
     def testInitClass(self):
-        newNode =  Node(1, 'node 1', 0, [])
+        newNode =  Node(1, NodeType.MARKET, 0, [])
         self.assertIsInstance(newNode, Node)
         
 if __name__ == '__main__':
