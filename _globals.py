e0f641a8ec63ddb4eb8755240ce31afaccce1f84
Max <lhmax55@gmail.com>
2023-03-06 22:14:24 -0500

    add terminalSize

--- _globals.py
+++ _globals.py
@@ -2,6 +2,8 @@ from dataObjects.move import Move
 from dataObjects.type import Type
 from dataObjects.genericMon import GenericMon
 from gameObjects.sections.player.player import Player
+import os
+terminalSize : os.terminal_size = os.get_terminal_size()
 types: dict[str, Type] = {}
 moves: dict[str, Move] = {}
 genericMons: dict[str | int, GenericMon] = {}
