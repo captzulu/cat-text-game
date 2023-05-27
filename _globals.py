5a0b17890ee2be298a4579d9b38d842611a2ea3c
Max <lhmax55@gmail.com>
2023-05-27 00:40:21 -0400

    switch to using shutil because of wierd error

--- _globals.py
+++ _globals.py
@@ -2,8 +2,9 @@ from dataObjects.move import Move
 from dataObjects.type import Type
 from dataObjects.genericMon import GenericMon
 from gameObjects.sections.player.player import Player
+import shutil
 import os
-terminalSize : os.terminal_size = os.get_terminal_size()
+terminalSize : os.terminal_size = shutil.get_terminal_size()
 types: dict[str, Type] = {}
 moves: dict[str, Move] = {}
 genericMons: dict[str | int, GenericMon] = {}
