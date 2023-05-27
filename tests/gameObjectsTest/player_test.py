89994d35559dc32c0a21c5ec808ada5c31eea881
Max <lhmax55@gmail.com>
2023-05-27 13:54:07 -0400

    Add tests for player Fixes #16

--- tests/gameObjectsTest/player_test.py
+++ tests/gameObjectsTest/player_test.py
@@ -26,6 +26,18 @@ class playerTest(unittest.TestCase):
         newPlayer.addItem('test')
         self.assertEqual(len(newPlayer.items), 1)
         
+    def testAddMon(self):
+        newPlayer = Player('test')
+        specificMon = SpecificMon(_globals.genericMons['1'], 1)
+        newPlayer.addMon(specificMon)
+        self.assertEqual(len(newPlayer.party.mons), 1)
+    
+    @mock.patch('builtins.input', return_value = '1')
+    def testChooseMon(self, mocked_instance):
+        #also tests pickGenericMon and pickSpecificMon
+        newPlayer = Player('test')
+        newPlayer.chooseMon(1)
+        self.assertEqual(len(newPlayer.party.mons), 1)
     
 if __name__ == '__main__':
     unittest.main()
