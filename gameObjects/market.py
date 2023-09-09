83f101e8c96373e87dfc5405a4f4303feda80da1
Max <lhmax55@gmail.com>
2023-09-09 19:54:00 -0400

    polish the market : check if player has gold + refactoring

--- gameObjects/market.py
+++ gameObjects/market.py
@@ -13,11 +13,27 @@ class Market:
     def marketMenu(self):
         if not hasattr(self,'selection'):
             self.generateSelection()
+        pickedOption : tuple[str, int] = self.pickFromSelection()
+        if pickedOption[0] == 'Back':
+            return
+        if self.processPayment(pickedOption):
+            self.giveItem(pickedOption)
+        
+        
+    def pickFromSelection(self) -> tuple[str, int]:
         choices = {index:item[0] for (index, item) in self.selection.items()}
-        pickedOption : tuple[str, int] = self.selection[menuFunctions.menuInt(choices)]
-        _globals.player.gold -= pickedOption[1]
+        return self.selection[menuFunctions.menuInt(choices)]
+    
+    def processPayment(self, pickedOption : tuple[str, int]) -> bool:
+        if _globals.player.gold >= pickedOption[1]: 
+            _globals.player.gold -= pickedOption[1]
+            print(f"You bought {pickedOption[0]} for {pickedOption[1]}g. You now have {_globals.player.gold}g")
+            return True
+        else:
+            print(f"You have {_globals.player.gold}g, you need {pickedOption[1]}g to buy {pickedOption[0]}")
+            return False
+            
+    def giveItem(self, pickedOption : tuple[str, int]):
         if pickedOption[0] == 'Level up':
             _globals.player.party.activeMon.levelUp()
-        elif pickedOption[0] == 'Back':
-            return
         
