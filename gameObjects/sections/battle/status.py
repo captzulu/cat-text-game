eee400ab9864091fed751c23763c0e03efcc7577
Max <lhmax55@gmail.com>
2023-09-17 11:17:22 -0400

    always do 1 dmg minimum with poison

--- gameObjects/sections/battle/status.py
+++ gameObjects/sections/battle/status.py
@@ -3,5 +3,5 @@ class Status():
 
     @staticmethod
     def poison(mon : SpecificMon):
-        hpLost = mon.loseMaxHealthPercent((100/12))
-        print(f"{mon.nickname} suffers from poison ({hpLost} hp)")
+        hpLost = mon.loseMaxHealthPercent(100/12)
+        print(f"{mon.nickname} suffers from poison ({hpLost if hpLost > 0 else 1} hp)")
